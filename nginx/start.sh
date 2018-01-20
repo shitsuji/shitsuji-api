a2bad5e7271a041829f6d40d4efb90d1624bccb2
Rafal2228 <rafal2228@gmail.com>
2018-01-20 21:25:38 +0100

    add nginx proxy ssl docker setup

---
+++ nginx/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp /usr/src/proxy.conf /etc/nginx/conf.d/proxy.conf
+
+echo "settings host ${PROXY_HOST}"
+if [ -n "${PROXY_HOST}" ]; then
+  TARGET_SERVICE=$PROXY_HOST
+fi
+
+echo "settings port ${PROXY_PORT}"
+if [ -n "${PROXY_PORT}" ]; then
+  TARGET_SERVICE="${TARGET_SERVICE}:${PROXY_PORT}"
+fi
+
+sed -i "s/{{PROXY_SERVICE}}/${PROXY_SERVICE}/g;" /etc/nginx/conf.d/proxy.conf
+
+echo "Updated service ${PROXY_SERVICE}"
+echo "Starting nginx..."
+nginx -g 'daemon off;'
